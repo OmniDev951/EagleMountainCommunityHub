a45f76b81e6ceeebf2662d9024ef31560c9050a3
OmniDev951 <ochsjax000@alpinesd.org>
2025-10-24 14:59:18 -0600

    Basic Project Setup

--- EMCommunityHub/asgi.py
+++ EMCommunityHub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for eagle_mountain_com_hub project.
+ASGI config for EMCommunityHub project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eagle_mountain_com_hub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EMCommunityHub.settings')
 
 application = get_asgi_application()
--- EMCommunityHub/wsgi.py
+++ EMCommunityHub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eagle_mountain_com_hub project.
+WSGI config for EMCommunityHub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eagle_mountain_com_hub.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EMCommunityHub.settings')
 
 application = get_wsgi_application()
